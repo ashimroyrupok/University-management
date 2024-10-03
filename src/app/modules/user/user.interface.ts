0fa003525373cc81352c02ab744906714f8d9d28
ashimroyrupok <ashimroyrupok@gmail.com>
2024-10-04 01:17:24 +0600

    crated user model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -2,7 +2,7 @@ export type TUser = {
   id: string;
   password: string;
   needPasswordChange: boolean;
-  role: 'admin' | 'user' | 'faculty';
+  role: 'admin' | 'student' | 'faculty';
   status: 'in-progress' | 'blocked';
   isDeleted: boolean;
 };
