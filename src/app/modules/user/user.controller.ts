47afe3db9dbab4995e473cc8a900c3571fa31b16
ashimroyrupok <ashimroyrupok@gmail.com>
2024-10-05 00:16:43 +0600

    crate catchAsync function for stop repeating trycatch

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,33 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { userServices } from './user.services';
 import sendResponse from '../../utils/SendRespose';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student: studentData } = req.body;
-    // console.log(studentData, 'student');
-    // const zodParsedData = studentValidationSchema.parse(studentData);
+const createStudent = catchAsync(async (req, res, next) => {
+  const { password, student: studentData } = req.body;
+  // console.log(studentData, 'student');
+  // const zodParsedData = studentValidationSchema.parse(studentData);
 
-    const result = await userServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
-    sendResponse(res, {
-      success: true,
-      message: 'Student is created succesfully',
-      statusCode: httpStatus.OK,
-      data: result,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+  const result = await userServices.createStudentIntoDB(password, studentData);
+  sendResponse(res, {
+    success: true,
+    message: 'Student is created succesfully',
+    statusCode: httpStatus.OK,
+    data: result,
+  });
+});
 
 export const userControllers = {
   createStudent,
