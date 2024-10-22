2c510c98af6c904465f8160b27d485883deac21b
ashimroyrupok <ashimroyrupok@gmail.com>
2024-10-22 16:31:27 +0600

    create interface for academicSemester

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,10 +1,26 @@
-import { Router } from "express";
+/* eslint-disable no-unused-vars */
+import { NextFunction, Request, Response, Router } from "express";
 import { userControllers } from "./user.controller";
+import { AnyZodObject } from "zod";
+import { studentValidations } from "../student/student.validation";
 
 
 const router = Router();
 
-router.post('/create-student', userControllers.createStudent);
+
+const studentValidate =(schema:AnyZodObject)=>{
+    return async(req:Request,res:Response,next:NextFunction)=>{
+        try {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const zodParsedData = await schema.parseAsync({ body: req.body });
+            next();
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+router.post('/create-student',studentValidate(studentValidations.CreateStudentValidationSchema), userControllers.createStudent);
 
 
 export const UserRoutes = router;
