4e14b940d0d741638064843904dabfa6e6f586c2
ashimroyrupok <ashimroyrupok@gmail.com>
2025-01-10 21:54:34 +0600

    added admin module and its functionality

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -4,6 +4,8 @@ import { UserRoutes } from '../modules/user/user.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
+import { FacultyRoutes } from '../modules/Faculty/faculty.route';
+import { AdminRoutes } from '../modules/Admin/admin.route';
 // import { UserRoutes } from '../modules/user/user.route';
 
 const router = Router();
@@ -14,20 +16,29 @@ const moduleRoutes = [
     route: UserRoutes,
   },
   {
-    path: '/student',
+    path: '/students',
     route: StudentRoutes,
+  },
+  {
+    path: '/faculties',
+    route: FacultyRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
   },
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
   },
   {
-    path:'/academic-faculty',
-    route:AcademicFacultyRoutes
-  },{
-    path:'/academic-department',
-    route:AcademicDepartmentRoutes
-  }
+    path: '/academic-faculty',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-department',
+    route: AcademicDepartmentRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
